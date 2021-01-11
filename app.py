b7fd20bc3742045765f9ed91bbbc7bcea104d149
jb2718 <jbattle2718@gmail.com>
2021-01-10 18:34:44 -0600

    setting up basic create, list, and delete routes for images resource

--- app.py
+++ app.py
@@ -1,1 +1,22 @@
 from flask import Flask
+
+app = Flask(__name__)
+
+images = []
+
+# POST /images  {location:}
+@app.route('/images', methods=['POST'])
+def create_image():
+    return 'creating image...'
+
+# GET /images
+@app.route('/images')
+def list_images():
+    return 'listing images...'
+
+# DELETE /images/<string:id>
+@app.route('/images/<string:id>', methods=['DELETE'])
+def delete_image(id):
+    return 'deleting image {}...'.format(id)
+
+app.run(port=5000)
